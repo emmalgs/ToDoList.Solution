ff2c8fba8be8e49db5efef017e3717e7fdbd4d62
Emma Gerigscott <elgerig@gmail.com>
2023-05-21 11:55:32 -0700

    use dotnet migrations to update db and create many-to-many relationship

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -8,5 +8,6 @@ namespace ToDoList.Models
     public int ItemId { get; set; }
     public int CategoryId { get; set; }
     public Category Category { get; set; }
+    public List<ItemTag> JoinEntities { get; }
   }
 }
